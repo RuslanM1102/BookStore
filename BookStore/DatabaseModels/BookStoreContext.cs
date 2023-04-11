d7d3eee06483d339481d020fedf4f1b64826ab66
Руслан Мамедов <118619671+RuslanM1102@users.noreply.github.com>
2023-04-11 14:10:47 +0300

    fix

---
+++ BookStore/DatabaseModels/BookStoreContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DatabaseModels
+{
+    static class BookStoreContext
+    {
+        private static BookStoreEntities _context;
+
+        public static BookStoreEntities GetContext()
+        {
+            if(_context == null)
+            {
+                _context = new BookStoreEntities();
+            }
+            return _context;
+        }
+    }
+}

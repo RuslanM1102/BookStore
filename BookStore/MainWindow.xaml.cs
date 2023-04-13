83bfedddc19fe330d762b3b3083f51b416eeb1d6
Руслан Мамедов <118619671+RuslanM1102@users.noreply.github.com>
2023-04-13 11:40:22 +0300

    Add placeholders

--- BookStore/MainWindow.xaml.cs
+++ BookStore/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
+using System.Linq;
 
 namespace BookStore
 {
@@ -26,6 +27,31 @@ namespace BookStore
         {
             e.Cancel = MessageBox.Show(this, "Вы уверены?", "Выход",
                 MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.Yes) == MessageBoxResult.No;
+        }
+
+        private void Login_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            LoginPlaceholder.Visibility = Login.Text.Length > 0 ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        private void Password_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordPlaceholder.Visibility = Password.Password.Length > 0 ? Visibility.Hidden : Visibility.Visible;
+        }
+
+        private void Image_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var darkTheme = new Uri("Styles/DarkTheme.xaml", UriKind.Relative);
+            var lightTheme = new Uri("Styles/LightTheme.xaml", UriKind.Relative);
+
+            ResourceDictionary resourceDict = Application.LoadComponent(new Uri("Styles/Dictionary.xaml", UriKind.Relative)) as ResourceDictionary;
+            PasswordPlaceholder.Content = resourceDict.MergedDictionaries[0].Source.OriginalString;
+            Uri newTheme = resourceDict.MergedDictionaries.Where(x => x.Source.OriginalString == lightTheme.OriginalString).Count() == 1 ? darkTheme : lightTheme;
+
+            resourceDict.MergedDictionaries.Clear();
+            resourceDict.MergedDictionaries.Add(Application.LoadComponent(newTheme) as ResourceDictionary);
+
+            LoginPlaceholder.Content = newTheme.OriginalString;
         }
     }
 }

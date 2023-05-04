92eab62d3b487c41d3d08d95aa3ffa153ddbe466
Руслан Мамедов <118619671+RuslanM1102@users.noreply.github.com>
2023-05-04 11:27:33 +0300

    pr 22 preparation

--- BookStore/Pages/MainMenuPages/BooksPage.xaml.cs
+++ BookStore/Pages/MainMenuPages/BooksPage.xaml.cs
@@ -2,10 +2,8 @@
 using System;
 using System.Linq;
 using System.Linq.Dynamic.Core;
-using System.Text;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media.Imaging;
+using BookStore.Modules;
 
 namespace BookStore.Pages.MainMenuPages
 {
@@ -18,6 +16,47 @@ namespace BookStore.Pages.MainMenuPages
         {
             InitializeComponent();
             ListBooks.ItemsSource = BookStoreDB.GetItemsSource(BookStoreDB.GetContext().Nomenclature);
+        }
+
+        private void UpdateBooks()
+        {
+            dynamic currentSource = BookStoreDB.GetItemsSource(BookStoreDB.GetContext().Nomenclature);
+            if (!string.IsNullOrWhiteSpace(SearchBox.Text))
+            {
+                currentSource = currentSource.Where("Name.Contains(@0)", SearchBox.Text);
+            }
+
+            if (CheckFormat.IsChecked == true)
+            {
+                currentSource = currentSource.Where("Format == @0", "Твёрдая обложка");
+            }
+
+            if(OrderBox.SelectedIndex == 0)
+            {
+                currentSource = currentSource.OrderBy("Name");
+            }
+            else
+            {
+                currentSource = currentSource.OrderBy("Name desc");
+            }
+
+            ListBooks.ItemsSource = currentSource;
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            PlaceholderController.SetVisibility(SearchBox.Text, SearchPlaceholder);
+            UpdateBooks();
+        }
+
+        private void OrderBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateBooks();
+        }
+
+        private void CheckFormat_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+
         }
     }
 }
